9383683286b43a22cb0b4c4b17ad651cac527aaf
daniel-web-developer <daniel_caixeta@protonmail.com>
2023-04-26 22:47:44 +0200

    errors actually done

--- static/script.js
+++ static/script.js
@@ -1,16 +1,22 @@
 const button = document.querySelector('#btn');
 const errors = document.querySelectorAll("[data-error]");
 const inputs = document.querySelectorAll("[data-in]");
+const resultYear = document.querySelector('#resultYear');
+const resultMonth = document.querySelector('#resultMonth');
+const resultDay = document.querySelector('#resultDay');
+
+const today = new Date();
+const thisDay = today.getDate();
+const thisMonth = today.getMonth() + 1;
+const thisYear = today.getFullYear();
 
 const classes = ["error", "error-color", "display-none"];
 
+function resultsWrong(){
+    return "--";
+}
 
 button.addEventListener('click', () => {
-    
-    const today = new Date();
-    const thisDay = today.getDate();
-    const thisMonth = today.getMonth() + 1;
-    const thisYear = today.getFullYear();
 
     let textDay = document.querySelector("#textDay");
     let inputDay = document.querySelector('#inputDay').value;
@@ -22,12 +28,17 @@ button.addEventListener('click', () => {
     let inputYear = document.querySelector('#inputYear').value;
     let iYear = document.querySelector('#inputYear');
 
+    let rY = thisYear - inputYear;
+    let rM = thisMonth - inputMonth;
+    let rD = thisDay - inputDay;
+
     inputs.forEach(input => {
         input.className = '';
     })
 
     textDay.classList.add("main-date-each");
     textMonth.classList.add("main-date-each");
+    textYear.classList.add("main-date-each");
 
     errors.forEach(error => {
         error.className = '';
@@ -38,6 +49,10 @@ button.addEventListener('click', () => {
     if(inputDay == ""){
         let day = document.querySelector("#errorEmptyDay");
 
+        rY = resultsWrong();
+        rM = resultsWrong();
+        rD = resultsWrong();
+
         day.classList.remove('display-none');
         textDay.classList.add('error-color');
         iDay.classList.add('error-border-color');
@@ -46,6 +61,10 @@ button.addEventListener('click', () => {
     if(inputMonth == ""){
         let month = document.querySelector("#errorEmptyMonth");
 
+        rY = resultsWrong();
+        rM = resultsWrong();
+        rD = resultsWrong();
+
         month.classList.remove('display-none');
         textMonth.classList.add('error-color');
         iMonth.classList.add('error-border-color');
@@ -53,6 +72,10 @@ button.addEventListener('click', () => {
 
     if(inputYear == ""){
         let year = document.querySelector("#errorEmptyYear");
+
+        rY = resultsWrong();
+        rM = resultsWrong();
+        rD = resultsWrong();
 
         year.classList.remove('display-none');
         textYear.classList.add('error-color');
@@ -65,6 +88,10 @@ button.addEventListener('click', () => {
 
     if(nDay < 0 || nDay > 31 || nDay === 0 && inputDay != ""){
         let day = document.querySelector("#errorDay");
+
+        rY = resultsWrong();
+        rM = resultsWrong();
+        rD = resultsWrong();
         
         day.classList.remove('display-none');
         textDay.classList.add('error-color');
@@ -73,6 +100,10 @@ button.addEventListener('click', () => {
     
     if(nMonth < 0 || nMonth > 12 || nMonth === 0 && inputMonth != ""){
         let month = document.querySelector("#errorMonth");
+
+        rY = resultsWrong();
+        rM = resultsWrong();
+        rD = resultsWrong();
         
         month.classList.remove('display-none');
         textMonth.classList.add('error-color');
@@ -82,12 +113,20 @@ button.addEventListener('click', () => {
     if(nYear === 0 && inputYear != ""){
         let year = document.querySelector("#errorYear");
 
+        rY = resultsWrong();
+        rM = resultsWrong();
+        rD = resultsWrong();
+
         year.classList.remove('display-none');
         textYear.classList.add('error-color');
         iYear.classList.add('error-border-color');
     }
 
     if(nYear > thisYear || nMonth > thisMonth && nYear >= thisYear || nDay > thisDay && nMonth >= thisMonth && nYear >= thisYear){
+        rY = resultsWrong();
+        rM = resultsWrong();
+        rD = resultsWrong();
+
         future = document.querySelector("#errorFuture");
         future.classList.remove('display-none');
 
@@ -106,6 +145,7 @@ button.addEventListener('click', () => {
         invalidDate.classList.remove('display-none');
     }
 
-    
-
+    resultYear.textContent=rY;
+    resultMonth.textContent=rM;
+    resultDay.textContent=rD;
 })
